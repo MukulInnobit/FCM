44ff8394dbb5e72af8d8e6fd3f8f3f8c52f47e04
MukulInnobit <mukul.agrawal@innobitsystems.com>
2023-06-26 17:30:07 +0530

    first commit

--- public/firebase-messaging-sw.ts
+++ public/firebase-messaging-sw.ts
@@ -3,6 +3,7 @@
  importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 import firebase from "firebase/app"
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { onBackgroundMessage } from 'firebase/messaging/sw';
 
  // Initialize the Firebase app in the service worker by passing the generated config
  const firebaseConfig = {
@@ -21,13 +22,34 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
  // Retrieve firebase messaging
  const messaging = getMessaging(app);
 
-//  messaging.onBackgroundMessage(function(payload) {
-//    console.log("Received background message ", payload);
+ onBackgroundMessage(messaging,function(payload:any) {
+   console.log("Received background message ", payload);
 
-//    const notificationTitle = payload.notification.title;
-//    const notificationOptions = {
-//      body: payload.notification.body,
-//    };
+   const notificationTitle = payload.notification.title;
+   const notificationOptions = {
+     body: payload.notification.body,
+   };
+  //  self?.registration.showNotification(notificationTitle, notificationOptions);
 
-//    self.registration.showNotification(notificationTitle, notificationOptions);
-//  });
+   if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker
+        .register('./service-worker.js', { scope: './' })
+        .then(function (registration) {
+            console.log("Service Worker Registered");
+            setTimeout(() => {
+                registration.showNotification(payload.data.title, {
+                    body: payload.data.body,
+                    data: payload.data.link
+                });
+                registration.update();
+            }, 100);
+        })
+        .catch(function (err) {
+            console.log("Service Worker Failed to Register", err);
+        })
+
+}
+
+    
+ });
